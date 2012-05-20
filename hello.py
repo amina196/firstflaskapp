b82c3af3f81ba2158e17dad182659940209b4706
Amina <amina.bouabdallah@gmail.com>
2012-05-20 15:46:33 -0400

    first commit

---
+++ hello.py
@@ -0,0 +1,41 @@
+import os
+from flask import Flask
+from flask import render_template
+from flask import request
+from flask import redirect, url_for
+
+from flask import Flask, session, redirect, url_for, escape, request
+
+app = Flask(__name__)
+app.debug = True
+
+@app.route('/')
+def index():
+    if 'username' in session:
+        return 'Logged in as %s' % escape(session['username'])
+    return 'You are not logged in'
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        session['username'] = request.form['username']
+        return redirect(url_for('index'))
+    return '''
+        <form action="" method="post">
+            <p><input type=text name=username>
+            <p><input type=submit value=Login>
+        </form>
+    '''
+
+@app.route('/logout')
+def logout():
+    # remove the username from the session if it's there
+    session.pop('username', None)
+    return redirect(url_for('index'))
+
+# set the secret key.  keep this really secret:
+app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
